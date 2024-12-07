7978cdfaa94859c768501cdf606e4e4f416c7560
Hibiki TOYAMA <e245742@ie.u-ryukyu.ac.jp>
2024-12-07 18:31:45 +0900

    step2

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,11 @@
 public class Report5 {
     public static void main(String[] args) {
+        try {
         String str = null;
         System.out.println(str.length());
+        } catch(NullPointerException e) {
+            System.out.println("NullPointerException発生!!");
+            System.out.println(e.getMessage());
+        }
     }
 }
